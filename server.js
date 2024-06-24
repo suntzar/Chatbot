48cd99a6565a599690fe48436895854d16d11dd1
L7 <84163272+suntzar@users.noreply.github.com>
2024-06-23 23:19:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('public')); // Serve arquivos estáticos da pasta 'public'
 
 const genAI = new GoogleGenerativeAI('AIzaSyAeGh0hB_nerwQfsx2UWN0pyZ0r8r3Gd7o');
 
-const generateContent = async (prompt) => {
+app.post('/mensagem', async (req, res) => {
+  const prompt = req.body.texto;
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
   try {
     const result = await model.generateContent(prompt);
     const response = await result.response;
-    return response.text();
+    const text = response.text();
+    res.json({ resposta: text });
   } catch (error) {
     console.error('Erro ao gerar conteúdo:', error);
-    throw error;
+    res.status(500).send('Erro ao processar a mensagem');
   }
-};
+});
 
-module.exports = generateContent;
+const PORT = Math.floor(Math.random() * (65535 - 1024 + 1)) + 1024;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}:`);
+  console.log(`localhost:${PORT}`);
+});
